c41769dca5627c37b2e0e98bc9ed0f158e9c7686
cyc1224 <46258697+cyc1224@users.noreply.github.com>
2022-06-21 21:07:56 +0800

    Update install.sh
    解决了一些bug

--- install.sh
+++ install.sh
@@ -1,36 +1,66 @@
-#!/bin/bash
+!/bin/bash
 echo "GDEI CHECKIN SCRIPT INSTALLER"
 dir=~/checkin@gdei
 if [ -d $dir ];then
     cd ~/checkin@gdei
 else
-    cd
+    cd 
     mkdir checkin@gdei
-    cd ~/checkin@gdei
+    cd checkin@gdei
 fi
+script='push.py'
 file='key.txt'
-if [ -e $file ];then
-    echo "PUSH SERVER ALREADY INSTALLED"
-else
-    wget https://github.com/cyc1224/checkin-gdei/checkin@gdei/push.py
-    python push.py
-fi
-file='id.txt'
-if[ -e $file ];then
-    echo "CHECKIN SERVER ALREADY INSTALLED"
-    exit 0
-else
-    read -p "ENTER USERNAME:" username
-    echo "密码明文保存，请在安全环境使用该脚本"
-    read -p "ENTER PASSWORD:" password
-    echo $username
-    $password > id.txt
-    script='linux.py'
-    if[ -e $script ];then
+if [ -e $script ];then
+    if [ -e $file ];then
+        echo "PUSH SERVER ALREADY INSTALLED"
     else
-    wget https://github.com/cyc1224/checkin-gdei/checkin@gdei/linux.py
-    sed -i "s#$username$#$username#g" linux.py
-    sed -i "s#$password$#$password#g" linux.py
-    python linux.py
+        echo > key.txt
+        python push.py
+    fi
+else
+    wget https://raw.githubusercontent.com/cyc1224/checkin-gdei/main/checkin@gdei/push.py
+    if [ -e $file ];then
+        echo "PUSH SERVER INSTALLED"
+    else
+        echo > key.txt
+        python push.py
+    fi
 fi
-exit 0
+script='linux.py'
+file='id.txt'
+if [ -e $script ];then
+    if [ -e $file ];then
+        echo "CHECKIN SERVER ALREADY INSTALLED"
+        exit
+    else
+        read -p "ENTER USERNAME:" username
+        echo "密码明文保存，请在安全环境使用该脚本"
+        read -p "ENTER PASSWORD:" password
+        echo -e "$username\n$password" > id.txt
+        if [ -e $script ];then
+            sed -i 's/#username#/'"$username"'/g' linux.py
+            sed -i 's/#password#/'"$password"'/g' linux.py
+        else
+            echo "1"
+        fi
+    fi
+else
+    wget https://raw.githubusercontent.com/cyc1224/checkin-gdei/main/checkin@gdei/linux.py
+    if [ -e $file ];then
+        echo "CHECKIN SERVER INSTALLED"
+        exit
+    else
+        read -p "ENTER USERNAME:" username
+        echo "密码明文保存，请在安全环境使用该脚本"
+        read -p "ENTER PASSWORD:" password
+        echo -e "$username\n$password" > id.txt
+        if [ -e $script ];then
+            sed -i 's/#username#/'"$username"'/g' linux.py
+            sed -i 's/#password#/'"$password"'/g' linux.py
+        else
+            echo "1"
+        fi
+    fi
+fi
+echo "安装成功，请手动执行python ~/checkin@gdei/linux.py"
+exit
